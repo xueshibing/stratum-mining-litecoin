55ec0a8b2ccc87e701fb39ae87244714cfe88231
Generalfault <generalfault@mentaljynx.com>
2012-11-11 09:17:44 -0600

    Checking for Bitcoind connect and sanity..

--- mining/__init__.py
+++ mining/__init__.py
@@ -1,6 +1,9 @@
 from service import MiningService
 from subscription import MiningSubscription
 from twisted.internet import defer
+import time
+import simplejson as json
+from twisted.internet import reactor
 
 @defer.inlineCallbacks
 def setup(on_startup):
@@ -11,8 +14,12 @@ def setup(on_startup):
     *before* you call setup() in the launcher script.'''
 
     from stratum import settings
-    from interfaces import Interfaces
-    
+
+    # Get logging online as soon as possible
+    import stratum.logger
+    log = stratum.logger.get_logger('mining')
+
+    from interfaces import Interfaces    
     # Let's wait until share manager and worker manager boot up
     (yield Interfaces.share_manager.on_load)
     (yield Interfaces.worker_manager.on_load)
@@ -28,6 +35,34 @@ def setup(on_startup):
                              settings.BITCOIN_TRUSTED_USER,
                              settings.BITCOIN_TRUSTED_PASSWORD)
     
+    # Check bitcoind
+    # 	Check we can connect (sleep)
+    # Check the results:
+    #	- getblocktemplate is avalible	(Die if not)
+    #	- we are not still downloading the blockchain	(Sleep)
+    log.info("Connecting to bitcoind...")
+    while True:
+	try:
+	    result = (yield bitcoin_rpc.getblocktemplate())
+	    if isinstance(result, dict):
+		if result['version'] == 2:
+		    break
+	except Exception, e:
+	    if isinstance(e[2], str):
+		if isinstance(json.loads(e[2])['error']['message'],str):
+		    error = json.loads(e[2])['error']['message']
+		    if error == "Method not found":
+			log.error("Bitcoind does not support getblocktemplate!!! (time to upgrade.)")
+			reactor.stop()
+		    elif error == "Bitcoin is downloading blocks...":
+			log.error("Bitcoind downloading blockchain... will check back in 30 sec")
+			time.sleep(29)
+		    else:
+			log.error("Bitcoind Error: %s", error)
+	time.sleep(1)		# If we didn't get a result or the connect failed
+    log.info('Connected to bitcoind - Ready to GO!')
+
+    # Start the coinbaser
     coinbaser = SimpleCoinbaser(bitcoin_rpc, settings.CENTRAL_WALLET)
     (yield coinbaser.on_load)
     
@@ -47,7 +82,6 @@ def setup(on_startup):
     # mechanism is not working properly    
     BlockUpdater(registry, bitcoin_rpc)
     
-    import stratum.logger
-    log = stratum.logger.get_logger('mining')
     log.info("MINING SERVICE IS READY")
-    on_startup.callback(True)  
+    on_startup.callback(True)
+
